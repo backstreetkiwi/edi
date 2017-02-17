601a44133e9c01a81124684302d40ee8d7f2406e
backstreetkiwi <mail@nikolaus-winter.de>
2017-02-17 22:38:29 +0100

    log4j 2

--- edi-server/src/main/java/de/zaunkoenigweg/edi/server/EdiBootstrap.java
+++ edi-server/src/main/java/de/zaunkoenigweg/edi/server/EdiBootstrap.java
@@ -1,21 +1,32 @@
 package de.zaunkoenigweg.edi.server;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 
 public class EdiBootstrap {
     
-    private final static Log LOG = LogFactory.getLog(EdiBootstrap.class);
-
+    private static final String SYSTEM_PROPERTY_LOG_DIR = "edi.log.dir";
+    
     public static void main(String[] args) {
-        LOG.info("EDI bootstrap started...");
-        LOG.info("Spring context successfully initialized.");
+        System.out.println("EDI: bootstrap started...");
+        if(StringUtils.isBlank(System.getProperty(SYSTEM_PROPERTY_LOG_DIR))) {
+            System.out.printf("EDI: system property '%s' is not set.%n", SYSTEM_PROPERTY_LOG_DIR);
+            System.exit(1);
+        }
+        Path logDir = Paths.get(System.getProperty(SYSTEM_PROPERTY_LOG_DIR));
+        if(!Files.exists(logDir) || !Files.isDirectory(logDir)) {
+            System.out.printf("EDI: directory '%s' does not exist.%n", SYSTEM_PROPERTY_LOG_DIR);
+            System.exit(1);
+        }
         AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(SpringContext.class);
         EdiServer server = springContext.getBean(EdiServer.class);
         server.run();
         springContext.close();
-        LOG.info("EDI bootstrap terminated.");
+        System.out.println("EDI: bootstrap terminated.");
     }
 }
